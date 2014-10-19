c002c97d3d89816fe056e304c1a8e469f7d78711
Morten Noddeland <morten@noddeland.no>
2014-10-19 23:28:10 +0200

    Added assignment 8

---
+++ assignment8/3/stopwatch.js
@@ -0,0 +1,57 @@
+var s,
+Stopwatch = {
+  settings: {
+    display: document.getElementById('display-area'),
+    toggleButton: document.getElementById('toggle-button'),
+    resetButton: document.getElementById('reset-button'),
+    timer: null,
+    stopTime: 0,
+    startTime: 0,
+    started: false
+  },
+
+  init: function() {
+    s = this.settings;
+
+    s.toggleButton.addEventListener('click', this.startStopTimer);
+    s.resetButton.addEventListener('click', this.resetTimer);
+  },
+
+  startStopTimer: function() {
+    if (s.started === false) {
+      s.started = true;
+      s.startTime = new Date().getTime();
+      s.timer = setInterval(Stopwatch.updateDisplay,1);
+    }
+
+    else {
+      s.started = false;
+      s.stopTime += new Date().getTime() - s.startTime;
+      clearInterval(s.timer);
+    }
+  },
+
+  resetTimer: function() {
+    s.started = false;
+    s.startTime = 0;
+    s.stopTime = 0;
+    clearInterval(s.timer);
+    s.display.innerHTML = "00:00:00.000";
+  },
+
+  updateDisplay: function() {
+    var newTime = new Date().getTime() - s.startTime + s.stopTime;
+    s.display.innerHTML = Stopwatch.formatDisplayTime(newTime);
+  },
+
+  formatDisplayTime: function(time) {
+    var ms = time % 1000;
+    time = (time - ms) / 1000;
+    var s = time % 60;
+    time = (time - s) / 60;
+    var mins = time % 60;
+    var hrs = (time - mins) / 60;
+
+    return (('00' + hrs).slice(-2)) + ':' + (('00' + mins).slice(-2)) + ':' + (('00' + s).slice(-2)) + '.' + (('00' + ms).slice(-3));
+  }
+};
